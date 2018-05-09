a8251772afa297b1d75ab8f3161f07089bf1a15e
Aleksander Fret <alemik@op.pl>
2018-05-09 20:39:24 +0200

    second requirements - cat clicker premium

--- src/js/CatClicker.js
+++ src/js/CatClicker.js
@@ -3,62 +3,106 @@ const CatClicker = (() => {
 
   const _ = instance => priv.get(instance);
 
+  const createCatLink = (cat, index) => {
+    const newItem = document.createElement('li');
+    const newLink = document.createElement('button');
+    newLink.classList.add('cat-button');
+    newLink.setAttribute('type', 'button');
+    newLink.setAttribute('data-index', index);
+    newLink.textContent = cat.name;
+
+    newItem.appendChild(newLink);
+
+    return newItem;
+  };
+
+  const prepareCatsList = (instance) => {
+    const { cats, catsContainer } = _(instance);
+    const list = catsContainer.querySelector('.cat-list');
+    const catsDOMElements = document.createDocumentFragment();
+    cats.forEach((cat, index) => {
+      const newCat = createCatLink(cat, index);
+      catsDOMElements.appendChild(newCat);
+    });
+    list.appendChild(catsDOMElements);
+  };
+
+  const prepareCatImgBox = (instance) => {
+    const { catsContainer } = _(instance);
+
+    const catImgBox = document.createElement('div');
+    catImgBox.classList.add('cat-box');
+    const newCat = document.createElement('figure');
+    newCat.classList.add('cat');
+    const newCatImg = document.createElement('img');
+    newCatImg.classList.add('cat-img');
+    newCatImg.setAttribute('src', '');
+    newCatImg.setAttribute('data-index', '');
+    const newCatInfo = document.createElement('figcaption');
+    newCatInfo.classList.add('cat-info');
+    const newCatName = document.createElement('span');
+    newCatName.classList.add('cat-name');
+    newCatName.textContent = 'Cat name';
+    const newCatClicks = document.createElement('span');
+    newCatClicks.classList.add('cat-clicks');
+    newCatClicks.textContent = 'Clicks: 0';
+
+    newCatInfo.appendChild(newCatName);
+    newCatInfo.appendChild(newCatClicks);
+    newCat.appendChild(newCatImg);
+    newCat.appendChild(newCatInfo);
+    catImgBox.appendChild(newCat);
+
+    catsContainer.appendChild(catImgBox);
+  };
+
+  const prepareCatsProp = (cats) => {
+    const newCats = cats.map(cat => ({
+      name: cat,
+      url: `img//images//${cat}.jpg`,
+      clickNumber: 0,
+    }));
+    return newCats;
+  };
+
+  const setCatTagProp = (instance) => {
+    const { catsContainer } = _(instance);
+    _(instance).catImgTag = catsContainer.querySelector('.cat-img');
+    _(instance).catNameTag = catsContainer.querySelector('.cat-name');
+    _(instance).catClickTag = catsContainer.querySelector('.cat-clicks');
+  };
+
+  const updateCatInfo = (instance, name, clickNumber) => {
+    _(instance).catNameTag.textContent = name;
+    _(instance).catClickTag.textContent = `Clicks: ${clickNumber}`;
+  };
+
   const increaseClickNumber = (instance, index) => {
     if (index >= 0 && index < _(instance).cats.length) {
       _(instance).cats[index].clickNumber++;
     }
   };
 
-  const dispalyClikNumber = (instance, index, box) => {
-    const clickNumberContainer = box;
-    const newClickNumber = _(instance).cats[index].clickNumber;
-    clickNumberContainer.textContent = `Clicks: ${newClickNumber}`;
-  };
-
-  const clickCountHandler = (instance, index, box) => {
+  const clickCountHandler = (instance, index) => {
+    const cat = _(instance).cats[index];
     increaseClickNumber(instance, index);
-    dispalyClikNumber(instance, index, box);
+    updateCatInfo(instance, cat.name, cat.clickNumber);
   };
 
-  const createCat = (cat, index) => {
-    const newCatContainer = document.createElement('li');
-    const newCat = document.createElement('figure');
-    newCat.classList.add('cat');
-    const newCatImg = document.createElement('img');
-    newCatImg.classList.add('cat-img');
-    newCatImg.setAttribute('src', cat.url);
-    newCatImg.setAttribute('data-index', index);
-    const newCatInfo = document.createElement('figcaption');
-    newCatInfo.classList.add('cat-info');
-    const newCatName = document.createElement('span');
-    newCatName.classList.add('cat-name');
-    newCatName.textContent = cat.name;
-    const newCatClicks = document.createElement('span');
-    newCatClicks.classList.add('cat-clicks');
-    newCatClicks.textContent = `Clicks: ${cat.clickNumber}`;
-
-    newCatInfo.appendChild(newCatName);
-    newCatInfo.appendChild(newCatClicks);
-    newCat.appendChild(newCatImg);
-    newCat.appendChild(newCatInfo);
-    newCatContainer.appendChild(newCat);
-
-    return newCatContainer;
+  const loadCat = (instance, index) => {
+    const { catImgTag } = _(instance);
+    const cat = _(instance).cats[index];
+    catImgTag.setAttribute('src', `img//images//${cat.name}.jpg`);
+    catImgTag.setAttribute('data-index', index);
+    updateCatInfo(instance, cat.name, cat.clickNumber);
   };
 
-  const prepareCatsDOM = (instance) => {
-    const { cats, catsContainer } = _(instance);
-    const catsDOMElements = document.createDocumentFragment();
-    cats.forEach((cat, index) => {
-      const newCat = createCat(cat, index);
-      catsDOMElements.appendChild(newCat);
-    });
-    catsContainer.appendChild(catsDOMElements);
-  };
-
-  const prepareCatsProp = (cats) => {
-    const newCats = cats.map(cat => ({ ...cat, clickNumber: 0 }));
-    return newCats;
+  const setActiveButton = (element) => {
+    const previous = document.querySelector('.active-cat');
+    if (previous) {
+      previous.classList.remove('active-cat');
+    }
+    element.classList.add('active-cat');
   };
 
   class CatClickerClass {
@@ -66,16 +110,28 @@ const CatClicker = (() => {
       const privateProps = {
         cats: prepareCatsProp(cats),
         catsContainer,
+        catImgTag: null,
+        catNameTag: null,
+        catClickTag: null,
       };
+
       priv.set(this, privateProps);
-      prepareCatsDOM(this);
+
+      prepareCatsList(this);
+      prepareCatImgBox(this);
+      setCatTagProp(this);
+      loadCat(this, 0);
+      setActiveButton(document.querySelector('button[data-index="0"]'));
 
       this.catsContainer.addEventListener('click', (event) => {
         const { target } = event;
         if (target.nodeName === 'IMG') {
           const catIndex = target.dataset.index;
-          const clickBox = target.parentElement.querySelector('.cat-clicks');
-          clickCountHandler(this, catIndex, clickBox);
+          clickCountHandler(this, catIndex);
+        } else if (target.nodeName === 'BUTTON') {
+          const catIndex = target.dataset.index;
+          loadCat(this, catIndex);
+          setActiveButton(target);
         }
       });
     }
@@ -86,20 +142,33 @@ const CatClicker = (() => {
     get catsContainer() {
       return _(this).catsContainer;
     }
+    get catImgTag() {
+      return _(this).catImgTag;
+    }
+    get catNameTag() {
+      return _(this).catNameTag;
+    }
+    get catClickTag() {
+      return _(this).catClickTag;
+    }
   }
 
   return CatClickerClass;
 })();
 
 const cats = [
-  {
-    url: 'img/images/bingo.jpg',
-    name: 'Bingo',
-  },
-  {
-    url: 'img/images/Archibald.jpg',
-    name: 'Archibald',
-  },
+  'archibald',
+  'banjo',
+  'benjamin',
+  'bingo',
+  'chewie',
+  'duke',
+  'fabio',
+  'flash',
+  'homer',
+  'simba',
+  'swift',
+  'tiberius',
 ];
 
 const catClicker = new CatClicker( // eslint-disable-line no-unused-vars
